fc3b7784065a04a41dd6e234eccffff429316942
Miguel Campos <camposmiguel@gmail.com>
2023-09-27 14:10:08 +0200

    modal detail when click on pokemon

--- 04_api_get_request/index.js
+++ 04_api_get_request/index.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+
+
     $.ajax({
         url: 'https://pokeapi.co/api/v2/pokemon',
         type: 'GET'
@@ -6,9 +8,22 @@ $(document).ready(function () {
         var listadoPomemon = resp.results;
         listadoPomemon.forEach(function (pokemon) {
             // var template = '<p><h1>' + pokemon.name + '</h1></p>';
-            var template = `<p><h1>${pokemon.name}</h1></p>`;
+            var template = `<p><h1 class="pokemon" pokemonid="1">${pokemon.name}</h1></p>`;
             $('#lista-pokemon').append(template);
         });
+    });
+
+    $(document).on('click', '.pokemon', function () {
+        var pokemonId = $(this).attr('pokemonid');
+
+        $.ajax({
+            url: 'https://pokeapi.co/api/v2/pokemon/' + pokemonId,
+            type: 'GET'
+        }).done(function (resp) {
+
+        });
+
+        $('#modal-detail').show();
     });
 
 });
