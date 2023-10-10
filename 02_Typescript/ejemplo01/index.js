b755e5dc0de18ab7ead7fac177551a4a9388bdad
Miguel Campos <camposmiguel@gmail.com>
2023-10-10 12:19:03 +0200

    typescript ejemplos

---
+++ 02_Typescript/ejemplo01/index.js
@@ -0,0 +1,10 @@
+var mensaje = "Hola Mundo";
+var mensaje2 = "Hola Mundo 2";
+var a = 5;
+var b;
+b = 10;
+var suma = a + b;
+console.log(mensaje + ": " + suma);
+var apellidos = "Perez";
+var edad;
+var PI = 3.1416;
---
+++ 02_Typescript/ejemplo01/index.ts
@@ -0,0 +1,13 @@
+let mensaje = "Hola Mundo";
+var mensaje2 = "Hola Mundo 2";
+let a = 5;
+let b: number;
+b = 10;
+let suma = a + b;
+
+console.log(mensaje + ": " + suma);
+
+let apellidos: string = "Perez";
+let edad: number;
+
+const PI = 3.1416;
